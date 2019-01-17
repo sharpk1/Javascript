591fae9afaa5fe46116d4898593c3ea0dc50de27
sharpk1 <32557637+sharpk1@users.noreply.github.com>
2019-01-16 23:37:16 -0600

    Adding comments to the code

--- AdvancedTipCalculator.js
+++ AdvancedTipCalculator.js
@@ -1,13 +1,8 @@
-
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-
-
+// declaring the empty lists signified for John's tips and totals  
 var johnTips = [];
 var johnTotals =[];
 
+//creating the John object comprised of properties and functions
 var john = {
 
     bills: [124, 48, 268, 180, 42],
@@ -15,27 +10,27 @@ var john = {
         var tip;
         var total;
         for (i = 0; i < bill.length; i++){
-            if (bill[i] < 50){
+            if (bill[i] < 50){ //if the bill was below $50, calculate the tip as 20%
                 tip = bill[i] * 0.2;
                 total = tip + bill[i];
                 johnTips.push(tip);
                 johnTotals.push(total);
-            }else if (bill[i] >= 50 && bill[i] <= 200){
+            }else if (bill[i] >= 50 && bill[i] <= 200){ //if the bill was between $50 and $200, calculate the tip as 15%
                 tip = bill[i] * 0.15;
                 total = tip + bill[i];
                 johnTips.push(tip);
                 johnTotals.push(total);
-            }else if (bill[i] > 200){
+            }else if (bill[i] > 200){ //if the bill was above $200, calculate the tip as 10%
                 tip = bill[i] * 0.15;
                 total = tip + bill[i];
-                johnTips.push(tip);
-                johnTotals.push(total);
+                johnTips.push(tip); //append to tips list
+                johnTotals.push(total); //append total to totals list
             }
                         
         }
         return johnTips;
     },
-    calcAverage: function(tip){
+    calcAverage: function(tip){ //calculating the average of tips
         var sum = 0;
         var average;
         for (i = 0; i < tip.length; i++){
@@ -43,30 +38,30 @@ var john = {
             average = sum / tip.length;
             
         }
-        this.average = average;
+        this.average = average; //declaring a new property in the John object
         return average;
     }
 
 }
 
-john.calcTip(john.bills);
-john.calcAverage(johnTips);
+john.calcTip(john.bills); // calculate the tip and using john.bills as argument passed in
+john.calcAverage(johnTips); // calculate the average of the tips, using the tips list passed in
 console.log("John's tip average is " + john.average);
 //console.log(john.calcAverage(johnTips));
 console.log("John's tips are " + johnTips);
 console.log("John's totals are " + johnTotals);
 
-
+// declaring the empty lists signified for Mark's tips and totals  
 var markTips = []
 var markTotals = []
 
 var mark = {
 
-    bills: [77, 375, 110, 45],
+    bills: [77, 375, 110, 45], // Mark's list of restaurant bills 
     calcTip: function(bill){
         var tip;
         var total;
-        for (i = 0; i < bill.length; i++){
+        for (i = 0; i < bill.length; i++){ // Mark has different rules than John on how he tips. Same function as John regardless
             if (bill[i] < 100){
                 tip = bill[i] * 0.2;
                 total = tip + bill[i];
@@ -107,7 +102,9 @@ console.log("Mark's tip average is " + mark.average);
 console.log("Mark's tips are " + markTips);
 console.log("Mark's totals are " + markTotals);
 
-if (john.average > mark.average){
+
+// comparing the averages of john vs mark
+if (john.average > mark.average){ 
     console.log("John paid more than Mark in tips");
 }else if (mark.average > john.average){
     console.log("Mark paid more than John in tips");
